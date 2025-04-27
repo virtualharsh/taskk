0eea65c86d724ae34d2311089ee21eb37826f2c3
harsh <harshsonegrahere@gmail.com>
2025-04-27 16:12:22 +0530

    server configured

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const dovenv = require('dotenv')
 
 dovenv.config()
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.get('/',(req,res)=>{
+  res.send('Hello World');
+})
+
 app.listen(PORT , (err)=>{
   if(err) console.log(err);
-  else console.log(`Server running : ${PORT}`);
+  else console.log(`Server running : http://localhost:${PORT}`);
 })
