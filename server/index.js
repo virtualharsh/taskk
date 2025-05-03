6b0c8c7722523eacb90153f1f27b779f6b3d04fd
harsh <harshsonegrahere@gmail.com>
2025-05-03 18:30:50 +0530

    Added backend packages

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ dovenv.config()
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.get('/',(req,res)=>{
-  res.send('Hello World');
+app.get('/', (req, res) => {
+    res.send('Hello World');
 })
 
-app.listen(PORT , (err)=>{
-  if(err) console.log(err);
-  else console.log(`Server running : http://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log(`Server running : http://localhost:${PORT}`);
 })
