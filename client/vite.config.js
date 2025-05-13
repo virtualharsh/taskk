b1f35e2a95482f69dbc6cc90f6e1e1ee7f6d246c
harsh <harshsonegrahere@gmail.com>
2025-05-13 15:59:15 +0530

    home redirect on cookie found

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/', // important for SPA routing
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
--- server/index.js
+++ server/index.js
@@ -1,31 +1,40 @@
-const express = require('express')
-const dovenv = require('dotenv')
-const cors = require('cors')
-const connectMongo = require('./connection')
-const authRouter = require('./routes/auth.router')
-const staticRouter = require('./routes/static.router')
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectMongo = require('./connection');
+const authRouter = require('./routes/auth.router');
+const staticRouter = require('./routes/static.router');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-dovenv.config()
-const PORT = process.env.PORT || 5000
-const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 app.use(cors({
-    origin: true,// Allow frontend URL
-    credentials: true // Allow cookies to be sent
+    origin: true,
+    credentials: true
 }));
+
 app.use(cookieParser());
-app.use('/', staticRouter)
-app.use('/api/auth/', authRouter)
 
+app.use('/', staticRouter);
+app.use('/api/auth/', authRouter);
 
-connectMongo(process.env.MONGODB_URI)
+// Serve static files from React app
+const root = path.join(__dirname, "..", "client", "dist"); // Adjust path
+app.use(express.static(root));
 
-app.listen(PORT, (err) => {
-    if (err) console.log(err);
-    else console.log(`Server running : http://localhost:${PORT}`);
-})
+// Catch-all route (for React Router)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(root, "index.html"));
+});
+
+connectMongo(process.env.MONGODB_URI);
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
