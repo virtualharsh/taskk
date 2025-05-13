013d4eed89822a4cffd3806f00f1b05158911cdf
harsh <harshsonegrahere@gmail.com>
2025-05-13 15:18:14 +0530

    added cors in server

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://taskk-1-16pw.onrender.com/'],// Allow frontend URL
+    origin: true,// Allow frontend URL
     credentials: true // Allow cookies to be sent
 }));
 app.use(cookieParser());
