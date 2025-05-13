c7c36ca20f81a16412ddd0ac3ad8a300fb6d5792
harsh <harshsonegrahere@gmail.com>
2025-05-13 15:32:20 +0530

    modified static router for /homoe

--- server/routes/static.router.js
+++ server/routes/static.router.js
@@ -4,9 +4,9 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 const staticRouter = express.Router()
 
-staticRouter.get('/home', authMiddleware, (req, res) => {    
-    res.json({ user: req.user });
-});
+// staticRouter.get('/home', authMiddleware, (req, res) => {    
+//     res.json({ user: req.user });
+// });
 
 staticRouter.post("/logout", (req, res) => {
     res.clearCookie("authToken");
