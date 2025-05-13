2f8f025c3a105f25f9d00ca77c81ae25e48bf699
harsh <harshsonegrahere@gmail.com>
2025-05-13 18:43:39 +0530

    fixed routing issue in home and login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,6 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter >
         <ThemeProvider>
-
             <App />
         </ThemeProvider>
         </BrowserRouter >
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 import { toast } from "sonner";
 
 const Home = () => {
-    const SERVER = import.meta.env.VITE_SERVER; 
-    
+    const SERVER = import.meta.env.VITE_SERVER;
+
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
+
     useEffect(() => {
-        const verifyUser = async () => {
+        const fetchUser = async () => {
             try {
-                const response = await axios.get(`${SERVER}/home`, { withCredentials: true });                
-                setUser(response.data.user);
+                const response = await axios.get(`${SERVER}/home`, { withCredentials: true });
+                setUser(response?.data?.user);
 
             } catch (error) {
-                toast.error(error?.response?.data?.message);
-                navigate("/login");
+                toast.error("User not authenticated; Please login");
+                console.log(error);
             }
-        };
+        }
 
-        verifyUser();
+        fetchUser();
+
+        const token = Cookies.get('authToken') || null
+
+        if (token == null) {
+            toast.error('Please login to continue')
+            navigate('/login')
+        }
     }, []);
     return (
         <div>Home, Welcome {user}</div>
